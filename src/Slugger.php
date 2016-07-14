494c9926d54e2eddb16bcaa262dca4c763014843
Jakob Steinn <jstoone@drk.sh>
2016-07-14 02:05:02 +0200

    Adding code.

---
+++ src/Slugger.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace JakobSteinn\SolidSlugger;
+
+/**
+ * @author      Jakob Steinn <jstoone@drk.sh>
+ * @license     MIT
+ */
+class Slugger {
+
+    protected $characters = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+
+        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+
+        '-', '_', '.', '!', '~', '*', '\'', '(', ')',
+    ];
+
+    protected $incrementalBase;
+
+    public function __construct() {
+        $this->incrementalBase = count($this->characters);
+    }
+
+    public function encode($value) {
+        $result = '';
+
+        if($value == 0) {
+            return $this->characters[0];
+        }
+
+        while($value > 0) {
+            $result .= $this->characters[$value % $this->incrementalBase];
+
+            $value = floor($value / $this->incrementalBase);
+        }
+
+        return $result;
+    }
+
+    public function decode($value) {
+        $valueLength = count($value);
+        $result = 0;
+
+        for($currentCharacterIndex = 0; $currentCharacterIndex < $valueLength; $currentCharacterIndex++) {
+
+            $currentCharacter = $value[$currentCharacterIndex];
+
+            $currentCharacterValue = array_search($currentCharacter, $this->characters, true);
+
+            $result += pow($this->incrementalBase, $valueLength - 1 - $currentCharacterIndex) * $currentCharacterValue;
+        }
+
+        return $result;
+    }
+}
+
---
+++ tests/unit/SluggerTest.php
@@ -0,0 +1,23 @@
+<?php
+use JakobSteinn\SolidSlugger\Slugger;
+
+class SluggerTest extends PHPUnit_Framework_TestCase {
+
+
+    /** @test */
+    public function it_can_encode_incremental_slugs() {
+        $slugger = new Slugger();
+
+        $this->assertEquals('Q', $slugger->encode(42));
+
+
+    }
+
+    /** @test */
+    public function it_can_decode_incremental_slugs() {
+        $slugger = new Slugger();
+
+        $this->assertEquals(42, $slugger->decode('Q'));
+    }
+
+}
